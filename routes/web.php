b06311ff43f72798acb8efa9bc79f5308c7ca848
vishwa dilshara <126236252+VishwaDilshara@users.noreply.github.com>
2026-06-14 19:22:23 +0530

    change route to address-import

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AddressImportController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/address-import');
 });
 
 Route::prefix('address-import')->name('address-import.')->group(function () {
